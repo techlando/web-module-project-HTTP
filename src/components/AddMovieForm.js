08745bd32c1da27571aabe18647820f6ae18ba19
Landon Phillips <landonphillips@Landons-MBP.attlocal.net>
2022-04-28 21:26:43 -0500

    finished mvp

--- src/components/AddMovieForm.js
+++ src/components/AddMovieForm.js
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useHistory, Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import {  useHistory, Link } from 'react-router-dom';
+
 
 
 import axios from 'axios';
 
-const EditMovieForm = (props) => {
+const AddMovieForm = (props) => {
 	const { push } = useHistory();
-	const { id } = useParams();
+	
 
 	
 
@@ -18,15 +19,7 @@ const EditMovieForm = (props) => {
 		metascore: 0,
 		description: ""
 	});
-	useEffect(() => {
-		axios.get(`http://localhost:9000/api/movies/${id}`)
-		.then(res => {
-			setMovie(res.data)
-		})
-		.catch(err => {
-			console.log(err)
-		})
-	},[])
+
 	
 	const handleChange = (e) => {
         setMovie({
@@ -37,10 +30,10 @@ const EditMovieForm = (props) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        axios.put(`http://localhost:9000/api/movies/${id}`, movie)
+        axios.post(`http://localhost:9000/api/movies`, movie)
             .then(res=>{
-                setMovies(res.data);
-                push(`/movies/${movie.id}`);
+                props.setMovies(res.data);
+                push(`/movies`);
 			})
 			.catch(err=>{
 				console.log(err);
@@ -54,7 +47,7 @@ const EditMovieForm = (props) => {
 		<div className="modal-content">
 			<form onSubmit={handleSubmit}>
 				<div className="modal-header">						
-					<h4 className="modal-title">Editing <strong>{movie.title}</strong></h4>
+					<h4 className="modal-title">Adding <strong>{movie.title}</strong></h4>
 				</div>
 				<div className="modal-body">					
 					<div className="form-group">
@@ -81,11 +74,11 @@ const EditMovieForm = (props) => {
 				</div>
 				<div className="modal-footer">			    
 					<input type="submit" className="btn btn-info" value="Save"/>
-					<Link to={`/movies`}><input type="button" className="btn btn-default" value="Cancel"/></Link>
+					<Link to={"/movies/add"}><input type="button" className="btn btn-default" value="Cancel"/></Link>
 				</div>
 			</form>
 		</div>
 	</div>);
 }
 
-export default EditMovieForm;
+export default AddMovieForm;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -20,6 +20,18 @@ const Movie = (props) => {
                 console.log(err.response);
             })
     }, [id]);
+    const handlClick = () => {
+        axios.delete(`http://localhost:9000/api/movies/${id}`)
+        .then(res => {
+            props.deleteMovie(id);
+            
+            push("/movies")
+            
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
 
     return(<div className="modal-page col">
         <div className="modal-dialog">
@@ -52,7 +64,7 @@ const Movie = (props) => {
                         <section>
                             <span className="m-2 btn btn-dark">Favorite</span>
                             <Link to={`/movies/edit/${movie.id}`} className="m-2 btn btn-success">Edit</Link>
-                            <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
+                            <span className="delete"><input  onClick={handlClick} type="button" className="m-2 btn btn-danger" value="Delete"/></span>
                         </section>
                     </div>
                 </div>
